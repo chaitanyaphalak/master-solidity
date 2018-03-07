198dd95bb08dc9813280f1f4e58429c270b3a577
chaitanyaphalak <chaitanya.phalak@gmail.com>
2018-03-07 01:17:23 -0700

    Added a solidity/ethereum smart contract for ToDo App to maintain tasks.

---
+++ ToDoApp/todo.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.20;
+
+contract ToDo {
+    struct Task {
+        string taskName;
+        string taskTime;
+    }
+    
+    event LogTaskAdded(uint taskId, string taskName, string taskTime);
+    mapping (uint => Task) private TaskMap;
+    uint[] private taskIdList;
+    
+    function addTask(uint _id, string _taskName, string _taskTime) public returns (bool) {
+        TaskMap[_id] = Task({
+            taskName: _taskName,
+            taskTime: _taskTime
+        });
+        taskIdList.push(_id);
+        LogTaskAdded(_id, _taskName, _taskTime);
+        return true;
+    }
+    
+    function getTask(uint id) public constant returns (string, string) {
+        return (TaskMap[id].taskName, TaskMap[id].taskTime);
+    }
+    
+    function getTasksCount() public constant returns (uint) {
+        return taskIdList.length;
+    } 
+}
